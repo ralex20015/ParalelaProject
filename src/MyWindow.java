adf259390bdea40bf6bd7abbd4e2760b69d350a4
Eduardo Robles <lozanoeduardo8777@gmail.com>
2023-05-02 21:19:15 -0600

    Add a Fork method but have bugs

--- src/MyWindow.java
+++ src/MyWindow.java
@@ -2,49 +2,62 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.security.SecureRandom;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ForkJoinPool;
 
 public class MyWindow extends JFrame implements ActionListener {
 
     //private final JTextField txtAmountOfNumbers;
-    private final JTextArea areaWords, areaOrderedWords;
-    private final JButton btnAdd, btnMerge, btnForkJoin, btnClean, btnExecuteService;
+    private JTextArea areaUnorderedWords, areaOrderedWords;
+    private JButton btnAdd, btnSequential, btnForkJoin, btnClean, btnExecuteService;
 
-    private final JLabel lblMerge, lblFork;
+    private JLabel lblExecutor, lblFork, lblSequential;
+    private String [] arrayOfWords;
+    private final int numberOfCores = 8;
+    private ForkJoinPool forkJoinPool= new ForkJoinPool(numberOfCores);
+
+    private boolean hasText;
 
     public MyWindow(){
-        //txtAmountOfNumbers = new JTextField();
-        areaWords = new JTextArea();
+        initComponents();
+        cleanAll();
+    }
+
+    public void initComponents(){
+        areaUnorderedWords = new JTextArea();
         areaOrderedWords = new JTextArea();
-        JScrollPane sp = new JScrollPane(areaWords);
+        JScrollPane sp = new JScrollPane(areaUnorderedWords);
         JScrollPane sp2 = new JScrollPane(areaOrderedWords);
         sp.setBounds(20,60,270,420);
         sp.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
         sp2.setBounds(340,60,270,420);
         sp2.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-        areaWords.setBackground(new Color(230,128,65));
+        areaUnorderedWords.setBackground(new Color(230,128,65));
         areaOrderedWords.setBackground(new Color(230,128,65));
         btnAdd = new JButton("Agregar datos");
-        btnMerge = new JButton("Secuencial");
-        btnMerge.setActionCommand("Add");
+        btnSequential = new JButton("Secuencial");
+        btnSequential.setActionCommand("Add");
         btnClean = new JButton("Limpiar");
         btnForkJoin = new JButton("Fork/Join");
         btnExecuteService = new JButton("Execute Service");
         JLabel label = new JLabel("Ingrese el texto: ");
-        lblMerge = new JLabel("as");
+        lblSequential = new JLabel("as");
         lblFork = new JLabel("asd");
+        lblExecutor = new JLabel("asdas");
 
+        //We set every component on the window
         label.setBounds(20,20,label.getPreferredSize().width+20,label.getPreferredSize().height);
-        lblMerge.setBounds(400,0,100,30);
+        lblSequential.setBounds(400,0,100,30);
         lblFork.setBounds(500,0,100,30);
         //txtAmountOfNumbers.setBounds(140,15,200,30);
         btnAdd.setBounds(20,500,btnAdd.getPreferredSize().width,btnAdd.getPreferredSize().height);
-        btnMerge.setBounds(150 ,500,btnMerge.getPreferredSize().width,btnMerge.getPreferredSize().height);
+        btnSequential.setBounds(150 ,500, btnSequential.getPreferredSize().width, btnSequential.getPreferredSize().height);
         btnClean.setBounds(245 ,500,btnClean.getPreferredSize().width,btnClean.getPreferredSize().height);
         btnForkJoin.setBounds(335 ,500,120,btnForkJoin.getPreferredSize().height);
         btnExecuteService.setBounds(470 ,500,btnExecuteService.getPreferredSize().width + 20,
                 btnExecuteService.getPreferredSize().height);
-        areaWords.setBounds(20,60,270,420);
+        areaUnorderedWords.setBounds(20,60,270,420);
         areaOrderedWords.setBounds(340,60,270,420);
 
         setSize(650,600);
@@ -54,38 +67,116 @@ public class MyWindow extends JFrame implements ActionListener {
         setLocationRelativeTo(null);
         setVisible(true);
 
-        btnMerge.addActionListener(this);
-       // addListeners();
+        btnAdd.addActionListener(this);
+        btnAdd.setActionCommand("Add");
+        btnSequential.addActionListener(this);
+        btnSequential.setActionCommand("Sequential");
+        btnForkJoin.addActionListener(this);
+        btnForkJoin.setActionCommand("Fork");
 
         add(label);
         //add(txtAmountOfNumbers);
         add(btnAdd);
-        add(btnMerge);
+        add(btnSequential);
         add(btnClean);
         add(btnForkJoin);
         add(btnExecuteService);
         add(sp);
         add(sp2);
-        add(lblMerge);
+        add(lblSequential);
         add(lblFork);
+    }
+
+    private void cleanAll(){
+        areaUnorderedWords.setText("");
+        areaOrderedWords.setText("");
+        lblExecutor.setText("");
+        lblFork.setText("");
+        lblSequential.setText("");
+    }
+
+    private void generateArrayOfWords(){
+        String text = JOptionPane.showInputDialog(null,
+                "Ingresa el texto que deseas:");
+        if(text!=null){
+            if(!text.equals("")){
+                arrayOfWords = text.split(" ");
+            }
+        }
+        setTextOnAreaComponent(areaUnorderedWords, arrayOfWords);
+    }
+
+    private void setTextOnAreaComponent(JTextArea textArea, String []arrayOfWords){
+        for (int i = 1; i < arrayOfWords.length; i++)
+        {
+            if (i % 5 == 0){
+                textArea.append("\n");
+            }
+            textArea.append(arrayOfWords[i - 1] + " ");
+        }
     }
 
 
     @Override
     public void actionPerformed(ActionEvent e) {
         String action = e.getActionCommand();
-
         if (action.equals("Add")){
-            System.out.println("Si entro");
-            String allText = areaWords.getText();
-            String[] arr = allText.split(" ");
-            Words words = new Words(arr);
-            words.orderWords();
-            String orderedWords = "";
-            for (int i = 0; i < arr.length; i++) {
-                orderedWords += arr[i]+" ";
-            }
-            areaOrderedWords.setText(orderedWords);
+            cleanAll();
+            generateArrayOfWords();
         }
+        if (action.equals("Sequential")){
+            cleanAll();
+            setTextOnAreaComponent(areaUnorderedWords, arrayOfWords);
+
+            String[] copy = arrayOfWords.clone();
+            lblSequential.setText(sequentialProcess(copy)+ " ns");
+            setTextOnAreaComponent(areaOrderedWords,copy);
+        }
+        if (action.equals("Fork")){
+            //cleanAll();
+            //setTextOnAreaComponent(areaUnorderedWords, arrayOfWords);
+            String[] copy = arrayOfWords.clone();
+            lblFork.setText(forkProcess(copy)+ " ns");
+            setTextOnAreaComponent(areaOrderedWords,copy);
+        }
+    }
+
+    private long sequentialProcess(String[] array)
+    {
+        long startTime = System.nanoTime();
+        Words words = new Words(array, 0, array.length - 1);
+        words.sort();
+        return System.nanoTime() - startTime;
+    }
+
+    private long executorProcess(String[] array)
+    {
+        long startTime = System.nanoTime();
+        Runnable r = () -> {
+            Words merge = new Words(array, 0, array.length - 1);
+            merge.sort();
+        };
+        ExecutorService executor = Executors.newFixedThreadPool(numberOfCores);
+        executor.execute(r);
+        return System.nanoTime() - startTime;
+    }
+
+
+    private long forkProcess(String[] array)
+    {
+        long startTime = System.nanoTime();
+
+        Runnable r = new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                Words words = new Words(array, 0, array.length - 1);
+                words.sort();
+            }
+        };
+        forkJoinPool.execute(r);
+
+        return System.nanoTime() - startTime;
     }
 }
--- src/Words.java
+++ src/Words.java
@@ -1,13 +1,104 @@
 import java.util.Arrays;
+import java.util.concurrent.RecursiveAction;
 
-public class Words {
-    private String [] words;
-    public Words(String [] words){
-        this.words = words;
+public class Words extends RecursiveAction {
+    private final String[] arrayOfWords;
+    private final int lo;
+    private final int hi;
+
+    public Words(String[] a, int lo, int hi)
+    {
+        this.arrayOfWords = a;
+        this.lo = lo;
+        this.hi = hi;
     }
 
-    public void orderWords(){
-        Arrays.sort(words,
-                String.CASE_INSENSITIVE_ORDER);
+    public void sort()
+    {
+        String temp;
+        for (int i = 0; i < arrayOfWords.length; i++) {
+            for (int j = i + 1; j < arrayOfWords.length; j++) {
+
+                // to compare one string with other strings
+                if (arrayOfWords[i].compareTo(arrayOfWords[j]) > 0) {
+                    // swapping
+                    temp = arrayOfWords[i];
+                    arrayOfWords[i] = arrayOfWords[j];
+                    arrayOfWords[j] = temp;
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void compute()
+    {
+        if (lo < hi)
+        {
+            int mid = lo + (hi - lo) / 2;
+            Words left = new Words(arrayOfWords, lo, mid);
+            Words right = new Words(arrayOfWords, mid + 1, hi);
+            invokeAll(left, right);
+            merge(this.arrayOfWords, this.lo, mid, this.hi);
+        }else {
+            return;
+        }
+    }
+
+    public void merge(String[] arr, int left, int middle, int right)
+    {
+        //Encuentra el tamaño de los sub-vectores para unirlos.
+        int n1 = middle - left + 1;
+        int n2 = right - middle;
+
+        //Vectores temporales.
+        String[] leftArray = new String[n1];
+        String[] rightArray = new String[n2];
+
+        //Copia los datos a los arrays temporales.
+        for (int i = 0; i < n1; i++)
+        {
+            leftArray[i] = arr[left + i];
+        }
+        for (int j = 0; j < n2; j++)
+        {
+            rightArray[j] = arr[middle + j + 1];
+        }
+
+        int i = 0, j = 0;
+
+        //Índice inicial del sub-vector arr[].
+        int k = left;
+
+        //Ordenamiento.
+        while (i < n1 && j < n2)
+        {
+            if (leftArray[i].charAt(0) <= rightArray[j].charAt(0))
+            {
+                arr[k] = leftArray[i];
+                i++;
+            } else
+            {
+                arr[k] = rightArray[j];
+                j++;
+            }
+            k++;
+        }//Fin del while.
+
+        /* Si quedan elementos por ordenar */
+        //Copiar los elementos restantes de leftArray[].
+        while (i < n1)
+        {
+            arr[k] = leftArray[i];
+            i++;
+            k++;
+        }
+        //Copiar los elementos restantes de rightArray[].
+        while (j < n2)
+        {
+            arr[k] = rightArray[j];
+            j++;
+            k++;
+        }
     }
 }
